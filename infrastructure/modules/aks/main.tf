6ceadd53b9457571aa82be81b6de356128b03fd9
BenYee15 <yee00034@algonquinlive.com>
2025-04-14 18:32:55 -0400

    Initial Terraform configuration for infrastructure provisioning Added `main.tf` with core infrastructure definitions Defined input variables in `variables.tf` for configurable deployments Created `outputs.tf` to expose key deployment outputs

---
+++ infrastructure/modules/aks/main.tf
@@ -0,0 +1,31 @@
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "${var.prefix}-aks-${var.environment}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = "${var.prefix}-aks-${var.environment}"
+
+  default_node_pool {
+    name           = "default"
+    node_count     = var.environment == "test" ? 1 : var.node_count
+    vm_size        = "Standard_B2s"
+    vnet_subnet_id = var.subnet_id
+    //enable_auto_scaling = true
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+
+  network_profile {
+    network_plugin = "azure"
+    service_cidr   = "10.240.0.0/16"
+    dns_service_ip = "10.240.0.10"
+  }
+
+
+
+  tags               = var.tags
+  kubernetes_version = "1.31.5"
+
+}
